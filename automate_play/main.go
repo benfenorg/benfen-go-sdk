0af68240562a22215386c8c5dd86acf1aca48345
wubin <18519247208@qq.com>
2025-11-06 15:40:46 +0800

    fix automate play

--- automate_play/main.go
+++ automate_play/main.go
@@ -15,9 +15,10 @@ func main() {
 	var abfc_address = "0xe2b59a41283f2efe5b5ed61aae23107504cc3ae67fd68b4ae0a6cec70a05dc58"
 	var to_address = "BFC64d767329da16653c62eb6b0e85bd5b7f0fd2f325ff2bedb7f00d54d2a2de38e5133"
 
-	var swap_poolid_usd = "BFCc1ebf7720a86b9387baf64e658fc2f8f242959e4d37b58cd6c59e7c334aa5355e383"
-	var abfc_usd_address = "BFC163e6d0652e0bd90460052f01499f511c849e81738b19176abdbbcdb0d5eb4d31b23"
-	var packageName = "0xb4b088e4d0d0f20d559e3f72178727cb38a21762cc758b96cf2973a5f1b31e8d::anonymous_usd::ANONYMOUS_USD"
+	//var swap_poolid_usd = "BFCc1ebf7720a86b9387baf64e658fc2f8f242959e4d37b58cd6c59e7c334aa5355e383"
+	//var abfc_usd_address = "BFC163e6d0652e0bd90460052f01499f511c849e81738b19176abdbbcdb0d5eb4d31b23"
+	//var packageName = "0xb4b088e4d0d0f20d559e3f72178727cb38a21762cc758b96cf2973a5f1b31e8d::anonymous_usd" +
+	//"::ANONYMOUS_USD"
 
 	for {
 		var cli, _ = client.Dial("http://localhost:9000")
@@ -42,8 +43,8 @@ func main() {
 
 		//cmd.GetAnonymousValue(
 		//	bfcPath,
-	//		abfc_address,
-	//	)
+		//		abfc_address,
+		//	)
 
 		cmd.SplitAndTransfer(bfcPath, abfc_address, to_address)
 		time.Sleep(5 * time.Second)
@@ -54,28 +55,23 @@ func main() {
 			client.FaucetFundAccount(address, LocalNetFaucetUrl)
 		}
 
-		objectid, _ = cli.GetCoinsRpc()
-		cmd.SwapInUsd(
-			bfcPath,
-			objectid,
-			swap_poolid_usd,
-			packageName,
-		)
-
-		cmd.SwapOutUsd(
-			bfcPath,
-			abfc_usd_address,
-			swap_poolid_usd,
-			packageName,
-		)
-
-	//	cmd.GetAnonymousValueUsd(
-	//		bfcPath,
-	//		abfc_usd_address,
-	//		packageName,
-	//	)
-
-		cmd.SplitAndTransferUsd(bfcPath, abfc_usd_address, to_address, packageName)
+		//objectid, _ = cli.GetCoinsRpc()
+		//cmd.SwapInUsd(
+		//	bfcPath,
+		//	objectid,
+		//	swap_poolid_usd,
+		//	packageName,
+		//)
+		//
+		//cmd.SwapOutUsd(
+		//	bfcPath,
+		//	abfc_usd_address,
+		//	swap_poolid_usd,
+		//	packageName,
+		//)
+		//
+		//
+		//cmd.SplitAndTransferUsd(bfcPath, abfc_usd_address, to_address, packageName)
 		time.Sleep(5 * time.Second)
 	}
 
